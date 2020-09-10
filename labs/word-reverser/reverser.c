6ac89fc74e67f80661abbd3eec06817929327ffa
Andre Calderon <andre.calder98@gmail.com>
2020-09-09 22:44:57 -0500

    Word Reverser Solution
    Signed-off-by: Andre Calderon <andre.calder98@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+#define IN 1
+#define OUT 0
+#define ARRAY_LENGTH 50
+
+void reverse(char* w){
+	int s = strlen(w);
+	char* a = w;
+	char* b = w + s - 1;
+
+	for(int i = 0; i<s/2; i++){
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		
+		a++;
+		b--;
+	}
 }
+
+int main(int argc, char* argv[]){
+	
+	char c;
+	char rev[ARRAY_LENGTH] = "";
+	char tmp;
+
+	while((c = getchar()) != EOF){
+		scanf("%s", rev);
+		if(c == '\n'){
+			reverse(rev);
+			printf("Reversed word: %s \n", rev);
+			rev[0] = '\0';
+		}
+	}
+	return 0;
+}
+
+
